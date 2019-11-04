5ed69e20309ac48864ea425076f99d1572a9fb39
Tuyen Pham <34194108+kimtuyen12@users.noreply.github.com>
2019-11-03 23:15:07 -0500

    update server.h

--- Project3/server.h
+++ Project3/server.h
@@ -11,16 +11,15 @@
 #include <unistd.h>
 
 // Defined constants
-#define BUFF_SIZE 256
-#define DEFAULT_DICTIONARY "dictionary.txt" // to be used if user does not specify a dictionary to use
-#define DEFAULT_LOG_FILE "logFile.txt"
 #define DEFAULT_PORT 7000 // to be used if no port specified by  user
-#define DICTIONARY_LENGTH 99171 /* how many lines are in provided dictionary */
+#define DEFAULT_DICTIONARY "dictionary.txt" // to be used if user does not specify a dictionary to use
+#define NUM_WORKER_THREADS 3 // number of worker threads to be created at start
+#define DICTIONARY_SIZE 200425 // size of dictionary file to be read in
 #define JOB_BUF_LEN 100 // size of job buffer which holds socket descriptors
 #define LOG_BUF_LEN 100 // size of log buffer
-#define MAX_SIZE 1000 // max word size possible in dictionary
-#define NUM_WORKERS 5 // number of worker threads to be created at start
-#define EXIT_NUM -1
+#define MAX_WORD_SIZE 15 // max word size possible in dictionary
+#define PHRASE_SIZE 25 // max phrase size - to be able to concatenate and include 'OK' or 'WRONG' on end of word
+
 
 /********* Function Declartion ***********/
 
@@ -32,8 +31,8 @@ int open_listenfd(int);
 /* both the worker thread function that prompts the user for their input,
   and checks the word against the dictionary. The log thread which is used
   to log all the activity from the clients requests to the server */
-void *worker_thread(void *);
-void *logger_thread(void *);
+void *workerThreadFunc(void *);
+void *loggerThreadFunc(void *);
 
 //search words from Dictionary.txt
 int searchForWordInDict(char list_of_words[][MAX_WORD_SIZE], char* wordToFind);
