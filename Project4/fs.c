adb0999153c1305e4402720d6dc455ed5e726992
Tuyen Pham <34194108+kimtuyen12@users.noreply.github.com>
2019-12-03 13:03:08 -0500

    create new fs.c with some functions

---
+++ Project4/fs.c
@@ -0,0 +1,100 @@
+#include <stddef.h>
+#include <printf.h>
+#include <memory.h>
+#include <stdlib.h>
+#include "file.h"
+#include "disk.h"
+
+super_block*    super_block_ptr;               // Super block
+file_info*      dir_info;                      // Directory info
+file_descriptor fd_table[MAX_FILE_DESCRIPTOR]; // File descriptor table
+
+int make_fs(char *disk_name)
+{
+    make_disk(disk_name);
+    open_disk(disk_name);
+
+    /* Initialize the super block */
+    super_block_ptr = (super_block*)malloc(sizeof(super_block));
+    if (super_block_ptr == NULL) return -1;
+    super_block_ptr->dir_index = 1;
+    super_block_ptr->dir_len = 0;
+    super_block_ptr->data_index = 2;
+
+    /* write the super block to disk (block 0) */
+    char buf[BLOCK_SIZE] = "";
+    // memset(void *b, int c, size_t len):
+    // writes len bytes of value c (converted to an unsigned char) to the string b.
+    memset(buf, 0, BLOCK_SIZE);
+    // memcpy(void *restrict dst, const void *restrict src, size_t n);
+    // copies n bytes from memory area src to memory area dst.
+    memcpy(buf, &super_block_ptr, sizeof(super_block));
+    block_write(0, buf);
+    free(super_block_ptr);
+
+    close_disk();
+    printf("make_fs()\t called successfully.\n");
+    return 0;
+}
+
+int mount_fs(char *disk_name)
+{
+    if(disk_name == NULL) return -1;
+    open_disk(disk_name);
+
+    /* read super block */
+    char buf[BLOCK_SIZE] = "";
+    memset(buf, 0, BLOCK_SIZE);
+    block_read(0, buf);
+    memcpy(&super_block_ptr, buf, sizeof(super_block_ptr));
+
+    /* read directory info */
+    dir_info = (file_info*)malloc(BLOCK_SIZE);
+    memset(buf, 0, BLOCK_SIZE);
+    block_read(super_block_ptr->dir_index, buf);
+    memcpy(dir_info, buf, sizeof(file_info) * super_block_ptr->dir_len);
+
+    /* clear file descriptors */
+    int i;
+    for(i = 0; i < MAX_FILE_DESCRIPTOR; ++i) {
+        fd_table[i].used = false;
+    }
+
+    printf("mount_fs()\t called successfully: file system [%s] mounted.\n", disk_name);
+    return 0;
+}
+
+int umount_fs(char *disk_name)
+{
+    if(disk_name == NULL) return -1;
+
+    /* write directory info */
+    int i, j = 0;
+    file_info* file_ptr = (file_info*)dir_info;
+    char buf[BLOCK_SIZE];
+    memset(buf, 0, BLOCK_SIZE);
+    char* block_ptr = buf;
+
+    for (i = 0; i < MAX_FILE; ++i) {
+        if(dir_info[i].used == true) {
+            memcpy(block_ptr, &dir_info[i], sizeof(dir_info[i]));
+            block_ptr += sizeof(file_info);
+        }
+    }
+
+    block_write(super_block_ptr->dir_index, buf);
+
+    /* clear file descriptors */
+    for(j = 0; j < MAX_FILE_DESCRIPTOR; ++j) {
+        if(fd_table[j].used == 1) {
+            fd_table[j].used = false;
+            fd_table[j].file = -1;
+            fd_table[j].offset = 0;
+        }
+    }
+
+    free(dir_info);
+    close_disk();
+    printf("umount_fs()\t called successfully: file system [%s] umounted.\n", disk_name);
+    return 0;
+}
