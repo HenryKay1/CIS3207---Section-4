2ae6e6efbcd2f180c18d0b566e2d1e4c50ddc021
Tuyen Pham <34194108+kimtuyen12@users.noreply.github.com>
2019-12-04 00:02:47 -0500

    update fs_close, fs_open and fs_create functions

--- Project4/fs.c
+++ Project4/fs.c
@@ -98,3 +98,66 @@ int umount_fs(char *disk_name)
     printf("umount_fs()\t called successfully: file system [%s] umounted.\n", disk_name);
     return 0;
 }
+
+int fs_open(char *name)
+{
+    char file_index = find_file(name);
+    if(file_index < 0) {  // file not found
+        fprintf(stderr, "fs_open()\t error: file [%s] does not exist.\n",name);
+        return -1;
+    }
+
+    int fd = find_free_file_des(file_index);
+    if (fd < 0){
+        fprintf(stderr, "fs_open()\t error: exceed the maximum file descriptor number.\n");
+        return -1;
+    }
+
+    dir_info[file_index].fd_count++;
+    printf("fs_open()\t called successfully: file [%s] opened.\n", name);
+    return fd;
+}
+
+int fs_close(int fildes)
+{
+    if(fildes < 0 || fildes >= MAX_FILE_DESCRIPTOR || !fd_table[fildes].used) {
+        return -1;
+    }
+
+    file_descriptor* fd = &fd_table[fildes];
+
+    dir_info[fd->file].fd_count--;
+    fd->used = false;
+
+    printf("fs_close()\t called successfully: file [%s] closed\n", dir_info[fd->file].name);
+    return 0;
+}
+
+int fs_create(char *name)
+{
+    char file_index = find_file(name);
+
+    if (file_index < 0){  // Create file
+        char i;
+        for(i = 0; i < MAX_FILE; i++) {
+            if(dir_info[i].used == false) {
+                super_block_ptr->dir_len++;
+                /* Initialize file information */
+                dir_info[i].used = true;
+                strcpy(dir_info[i].name, name);
+                dir_info[i].size = 0;
+                dir_info[i].head = -1;
+                dir_info[i].num_blocks = 0;
+                dir_info[i].fd_count = 0;
+                printf("fs_create()\t called successfully: file [%s] created.\n", name);
+                return 0;
+            }
+        }
+        fprintf(stderr, "fs_create()\t error: exceed the maximum file number.\n");
+        return -1;
+    } else {              // File already exists
+        fprintf(stderr, "fs_create()\t error: file [%s] already exists\n",name);
+        return 0;
+    }
+}
+
